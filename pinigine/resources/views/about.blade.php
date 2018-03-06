b545c974d69c3c0ff0176e0136b40dbdaae60f2e
vytautasphp <33592543+vytautasphp@users.noreply.github.com>
2018-03-06 19:35:29 +0200

    bandymai

---
+++ pinigine/resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    
+ 
+    <div>Apie</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sit sapiente cum, iure totam voluptatum, ab praesentium ratione nulla voluptates quasi! Unde molestias necessitatibus eaque molestiae quasi quaerat nam ex!</p>
+
+    @endsection
